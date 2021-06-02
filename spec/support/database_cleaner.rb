60f50b18d407d8580909448c703a98538545a9fc
Emilio <emilio2hd@gmail.com>
2021-06-02 18:26:47 +0100

    Remove db cleaner code related to capybara

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -31,15 +31,6 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, type: :feature) do
-    # :rack_test driver's Rack app under test shares database connection
-    # with the specs, so we can use transaction strategy for speed.
-    DatabaseCleaner.strategy = Capybara.current_driver == :rack_test ? :transaction : :truncation
-    # Non-:rack_test driver is probably a driver for a JavaScript browser
-    # with a Rack app under test that does *not* share a database
-    # connection with the specs, so we must use truncation strategy.
-  end
-
   config.before(:each) do
     DatabaseCleaner.start
   end
